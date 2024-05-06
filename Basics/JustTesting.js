f0ef94150743d00fa8c047b970538bef941fdaa8
billingCtrux <169078908+billingCtrux@users.noreply.github.com>
2024-05-07 00:14:00 +0530

    just changed teh comments forward slases

--- Basics/JustTesting.js
+++ Basics/JustTesting.js
@@ -1,3 +1,3 @@
-\\by testing the output in will be sure that the code is workig fine in the repository
+//by testing the output in will be sure that the code is workig fine in the repository
 var test = 'I am consoling this code just to check if the code is wokring fine of not';
 console.log(test);
